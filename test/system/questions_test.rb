e8ea2e9c1c247ebc9ffa3ea65d641755ab992622
Will Thompson <will.f.thompson@gmail.com>
2020-08-17 12:53:49 +0200

    confirmed testing

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -5,4 +5,12 @@ class QuestionsTest < ApplicationSystemTestCase
     visit ask_url
     assert_selector "p", text: "Ask your coach anything"
   end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
 end
